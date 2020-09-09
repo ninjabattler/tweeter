991f9f1d8885b87058be1ff5339c53c5f77a2ff0
ninjabattler <jaydenrtucker@gmail.com>
2020-09-09 21:44:51 +0000

    Added a function tha makes a get request ot /tweets and returns all tweets in the database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 const getDate = function(time){
 
@@ -73,30 +50,22 @@ const renderTweets = function(tweets) {
   }
 }
 
-
-  
+const loadTweets = function() {
+  $.ajax('/tweets', { method: 'GET' })
+    .then(function (tweets) {
+      console.log('Success: ', tweets);
+      renderTweets(tweets);
+    });
+}
 $(() =>{
-  renderTweets(data);
-
+  loadTweets();
   $('#tweetForm').on('submit', function (event) {
     event.preventDefault();
     console.log('Button clicked, performing ajax call...');
-    $.ajax($(this), { method: 'POST', data: $(this).serialize()})
+    $.ajax('/tweets', { method: 'POST', data: $(this).serialize()})
       .then(function () {
         console.log('Success: ', $(this).serialize());
-        data.unshift({
-          "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png"
-            ,
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": this.serialize()
-          },
-          "created_at": new Date().getTime()
-        });
-        renderTweets(data);
+        loadTweets();
       });
   });
 })
